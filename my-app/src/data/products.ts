a11a1c68d44279bdd369a0c3a31e043423a4ea73
Phumipas Namjaidee <phumihimal@gmail.com>
2024-11-03 21:22:20 +0700

    Add Navbar and every page

---
+++ my-app/src/data/products.ts
@@ -0,0 +1,31 @@
+type Product = {
+    id:Number,
+    name:string,
+    price:Number,
+    img:string,
+    category:string
+}
+
+const products:Product[] = [
+    {
+        id:1,
+        name:"Big Orca",
+        price:1000,
+        img:"/product_img/bigorca.png",
+        category:"Orca jub tong dai"
+    },
+    {
+        id:2,
+        name:"Orcahoi",
+        price:1000,
+        img:"/product_img/orcahoi.png",
+        category:"Orca jub tong dai"
+    },
+    {
+        id:3,
+        name:"Orcajub",
+        price:1000,
+        img:"/product_img/orcajub.png",
+        category:"Orca jub tong dai"
+    }
+]
